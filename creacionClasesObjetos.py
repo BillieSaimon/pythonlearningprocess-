67c27d578f48bcdc359b9986367a05742831c2d4
Simón Loyola Espinoza <131110769+BillieSaimon@users.noreply.github.com>
2024-04-27 08:05:27 -0400

    Programacion orientada a objetos primer ejercicio

---
+++ creacionClasesObjetos.py
@@ -0,0 +1,16 @@
+class Persona:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+
+persona1 = Persona('Juan', 'Perez', 28)
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
+
+persona1 = Persona('Karla', 'Garcia', 32)
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
